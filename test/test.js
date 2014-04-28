40ac7b46e419ad73e8b010599d199ed2bb4eb172
Kevin Martensson <kevinmartensson@gmail.com>
2014-04-28 08:28:23 +0200

    Use read-chunk

--- test/test.js
+++ test/test.js
@@ -2,13 +2,13 @@
 'use strict';
 
 var assert = require('assert');
-var fs = require('fs');
 var isPdf = require('../');
 var path = require('path');
+var readChunk = require('read-chunk');
 
 describe('isPdf()', function () {
     function check(file) {
-        return isPdf(fs.readFileSync(file));
+        return isPdf(readChunk.sync(file, 0, 4));
     }
 
     it('should detect PDF from buffer', function (cb) {
